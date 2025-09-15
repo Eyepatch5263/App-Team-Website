2cbd0b54ad14ad49dc497610401a042c66937646
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-09-15 23:08:47 +0530

    Add Google site verification meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+    <meta name="google-site-verification" content="wSkX_8y2bFPHN0i7D-DUVeKV2QAfnC2NVDFgCh0Cnhc" />
+    </head>
       <body>
         <Navbar />
           <main>{children}</main>
